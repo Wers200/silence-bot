f3b9f6854d7884a3a1c3dc7028c5c5382a25b48a
Wers200 <63037217+Wers200@users.noreply.github.com>
2020-07-02 12:45:14 +0300

    должно работать

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on('message', function(message) {
     client.user.setPresence({
         status: "dnd",
         activity: {
-            name: message.guild.id + ' members and ' + prefix + 'help',
+            name: message.guild.memberCount + ' members and ' + prefix + 'help',
             type: "WATCHING"
     	}
     });
