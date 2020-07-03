acd6c52f7d09c2a3ec0aaeca2548f59cd6a26993
Wers200 <63037217+Wers200@users.noreply.github.com>
2020-07-03 10:43:20 +0300

    дебаг (часть 2)

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ client.on('message', function(message) {
 		    	message.react('👍');
 		    	message.react('👎');
 	    	}
-		if(message.content.toLowerCase().startsWith(prefix + 'eval')) {
-			message.channel.send("```js\n" + eval(message.content.replace(prefix + 'idea ', '')) + "```");
+		if(message.content.toLowerCase().startsWith(prefix + 'eval') && message.member.id == "670559252456407070") {
+			message.channel.send("```js\n" + eval(message.content.replace(prefix + 'eval ', '')).toString() + "```");
 		} else if(message.content.toLowerCase().startsWith(prefix + 'help')) {
 			const answerEmbed = new discord.MessageEmbed()
 				.setColor('#c1e673')
