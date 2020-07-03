173bdbfd88de4d26ce55e1c6740640dee3db35dd
Wers200 <63037217+Wers200@users.noreply.github.com>
2020-07-03 12:14:12 +0300

    лол я дибил

--- index.js
+++ index.js
@@ -17,9 +17,6 @@ client.on('ready', function() {
 });
 
 client.on('message', function(message) {
-	if(message.guild.id != "723074367734546442") {
-		message.guild.leave();
-	}
 	try {
 		client.user.setPresence({
 			status: "dnd",
